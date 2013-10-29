aa854df915bd1a25153fa278c1cb5e75e3b8e15b
Milotskiy Alexey <spalaxinco@gmail.com>
2013-10-29 10:47:19 +0200

    Bug fix in change_project, added new behaviour

--- tools/change_project.sh
+++ tools/change_project.sh
@@ -15,7 +15,22 @@ while true; do
     esac
 done
 
-echo "Please enter new name for magento project : ";
+echo "Please enter OLD name for magento project or will've been used [milcrew] : ";
+read old_project_name
+while true; do
+    read -p "You've entered: [$old_project_name] does it correct ? [yes/no]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please try again from begining ..."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+if [ -z "${old_project_name}" ]; then
+   old_project_name="milcrew";
+fi
+
+echo "Please enter NEW name for magento project : ";
 read project_name
 while true; do
     read -p "You've entered: [$project_name] does it correct ? [yes/no]" yn
@@ -26,10 +41,86 @@ while true; do
     esac
 done
 
-echo "Changing database name in app/etc/local.xml" &&
-sed -i '' 's/milcrew/'$project_name'/g' $dir/app/etc/local.xml &&
-echo "Changing data in schema/cosmetiki.sql" &&
-sed -i '' 's/milcrew/'$project_name'/g' $dir/schema/milcrew.sql &&
 echo "Renaming default file milcrew.sql to $project_name.sql" &&
-mv $dir/schema/milcrew.sql $dir/schema/$project_name.sql &&
-echo "Done successfully";
+mv $dir/schema/$old_project_name.sql $dir/schema/$project_name.sql &&
+echo "Changing data in schema/$project_name.sql" &&
+sed -i '' 's/'$old_project_name'/'$project_name'/g' $dir/schema/$project_name.sql &&
+echo "Done files preparing successfully";
+
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+echo "Please enter your db name: ";
+read db_name
+while true; do
+    read -p "You've entered: [$db_name] does it correct ? [yes/no]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please try again from begining ..."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "Please enter your db user name: ";
+read db_user_name
+while true; do
+    read -p "You've entered: [$db_user_name] does it correct ? [yes/no]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please try again from begining ..."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "Please enter your db password: ";
+read db_user_password
+while true; do
+    read -p "You've entered: [$db_user_password] does it correct ? [yes/no]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please try again from begining ..."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+sed -i '' '/\<username\>\([^ ]*\)\<\/username\>/ s//<username><![CDATA['$db_user_name']]><\/username>/g' app/etc/local.xml &&
+sed -i '' '/\<password\>\([^ ]*\)\<\/password\>/ s//<password><![CDATA['$db_user_password']]><\/password>/g' app/etc/local.xml &&
+sed -i '' '/\<dbname\>\([^ ]*\)\<\/dbname\>/ s//<dbname><![CDATA['$db_name']]><\/dbname>/g' app/etc/local.xml && 
+echo "Changing credentials done successfull\n";
+
+if [ $? -ne 0 ]
+then
+   exit 1
+fi
+
+echo "Please put your mysql command or will've been used default [mysql]: ";
+read mysql_command
+while true; do
+    read -p "You've entered: [$mysql_command] does it correct ? [yes/no]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please try again from begining ..."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+if [ -z "${mysql_command}" ]; then
+  mysql_command="mysql" 
+fi
+
+
+$mysql_command -u $db_user_name -p$db_user_password -e "drop database if exists $db_name; create database if not exists $db_name;" &&
+$mysql_command -u $db_user_name -p$db_user_password $db_name < $dir/schema/$project_name.sql &&
+wget http://$project_name.in -O /dev/null &&
+$mysql_command -u $db_user_name -p$db_user_password -e "use $db_name;drop table xmlconnect_images;" &&
+$mysql_command -u $db_user_name -p$db_user_password $db_name < $dir/schema/$project_name.sql &&
+echo "Database prepared successfully\n";
+
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+echo "Everything done successfully";
