cf470ec102f34f83f83a4ebdd93c2e0bef02e43d
Milotskiy Alexey <spalaxinco@gmail.com>
2014-01-09 19:15:25 +0200

    Added sql schema with initial data and without

--- .Vagrantfilerc.rb
+++ .Vagrantfilerc.rb
@@ -9,5 +9,6 @@ module MyLocalVars
     MY_HOSTNAME = "demo-magento.in"
     MY_VBOX_NAME = "demo-magento-box"
     MY_VBOX_MEMORY = 1024
-
+    MY_INITIAL_SQL_FILE = "/var/www/schema/mage_dev_with_initial_data.sql"
+#mage_dev_empty.sql  mage_dev_with_initial_data.sql
 end
