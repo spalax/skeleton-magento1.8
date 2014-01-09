8b092b2a1dd9f63701a327dfe88912317d5cc25d
Milotskiy Alexey <spalaxinco@gmail.com>
2014-01-09 17:40:02 +0200

    Unify magento installation.

--- .Vagrantfilerc.rb
+++ .Vagrantfilerc.rb
@@ -6,8 +6,8 @@
 module MyLocalVars
 
     MY_IP = "192.168.99.170"
-    MY_HOSTNAME = "fedya-magento.com"
-    MY_VBOX_NAME = "magento-fedya-box"
+    MY_HOSTNAME = "demo-magento.in"
+    MY_VBOX_NAME = "demo-magento-box"
     MY_VBOX_MEMORY = 1024
 
 end
